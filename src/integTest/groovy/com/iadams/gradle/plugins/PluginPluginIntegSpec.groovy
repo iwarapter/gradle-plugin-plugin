96fefe2be5a512ab98fa5932dfc1ebbe231a61e3
Iain Adams <iwarapter@hotmail.com>
2015-12-07 17:40:58 +0000

    Added support for classpath manifest creation for integTests, also starting using the initial published version.

--- src/integTest/groovy/com/iadams/gradle/plugins/PluginPluginIntegSpec.groovy
+++ src/integTest/groovy/com/iadams/gradle/plugins/PluginPluginIntegSpec.groovy
@@ -1,3 +1,27 @@
+/*
+ * Gradle Plugin Plugin
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Iain Adams
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.iadams.gradle.plugins
 
 import com.iadams.gradle.plugins.utils.TestKitBaseIntegSpec
@@ -66,5 +90,70 @@ class PluginPluginIntegSpec extends TestKitBaseIntegSpec {
     then:
     result.task(':setupPlugin').outcome == SUCCESS
     file('src/main/groovy/org/other/MyPlugin.groovy').exists()
+  }
+
+  def "we can run an integration test"(){
+    when:
+    settingsFile << "rootProject.name = 'MyPlugin'"
+    def result = GradleRunner.create()
+        .withProjectDir(testProjectDir.root)
+        .withArguments('setupPlugin', 'licenseFormat', 'build')
+        .withPluginClasspath(pluginClasspath)
+        .build()
+
+    then:
+    result.task(':setupPlugin').outcome == SUCCESS
+
+    when:
+    buildFile << '''dependencies {
+  testCompile 'org.spockframework:spock-core:1.0-groovy-2.4', {
+    exclude module: 'groovy-all'
+  }
+}'''
+    file('src/integTest/groovy/com/example/ExampleIntegSpec.groovy') << ''' package com.example
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskOutcome.*
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class BuildLogicFunctionalTest extends Specification {
+    @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+    }
+
+    def "hello world task prints hello world"() {
+        given:
+        buildFile << """
+            task helloWorld {
+                doLast {
+                    println 'Hello world!'
+                }
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments('helloWorld')
+            .build()
+
+        then:
+        result.output.contains('Hello world!')
+        result.task(":helloWorld").outcome == SUCCESS
+    }
+}'''
+    result = GradleRunner.create()
+        .withProjectDir(testProjectDir.root)
+        .withArguments('integTest')
+        .withPluginClasspath(pluginClasspath)
+        .build()
+
+    then:
+    file('build/test-results/TEST-com.example.ExampleIntegSpec.xml').exists()
+    result.task(':integTest').outcome == SUCCESS
   }
 }
--- src/main/groovy/com/iadams/gradle/plugins/PluginPlugin.groovy
+++ src/main/groovy/com/iadams/gradle/plugins/PluginPlugin.groovy
@@ -1,3 +1,27 @@
+/*
+ * Gradle Plugin Plugin
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Iain Adams
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.iadams.gradle.plugins
 
 import com.gradle.publish.PublishPlugin
@@ -99,6 +123,19 @@ public class PluginPlugin implements Plugin<Project> {
     project.test.finalizedBy project.jacocoTestReport
     project.integTest.finalizedBy project.jacocoIntegTestReport
     project.integTest.finalizedBy project.jacocoCombinedTestReport
+
+
+    //TODO This is a temporary solution until a provide gradle alternative is available.
+    project.task('createClasspathManifest') {
+      def outputDir = project.file("${project.buildDir}/${project.name}")
+      inputs.files project.sourceSets.main.runtimeClasspath
+      outputs.dir outputDir
+      doLast {
+        outputDir.mkdirs()
+        project.file("$outputDir/plugin-classpath.txt").text = project.sourceSets.main.runtimeClasspath.join("\n")
+      }
+    }
+    project.dependencies.add('testRuntime',  project.files(project.tasks.findByName('createClasspathManifest')))
   }
 
   static void setupSonarQube(Project project) {
